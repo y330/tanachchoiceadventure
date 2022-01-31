15a6fa21a8c7b27803bf797945b24270c658cc8b
start.vaadin.com <no-reply@vaadin.com>
2022-01-31 13:56:19 +0000

    Generated project

---
+++ src/main/java/com/tanach/application/Application.java
@@ -0,0 +1,29 @@
+package com.tanach.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "tanachchoiceadventure")
+@PWA(name = "TanachChoiceAdventure", shortName = "TanachChoiceAdventure", offlineResources = {"images/logo.png"})
+@Push
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/tanach/application/views/rules/RulesView.java
@@ -0,0 +1,31 @@
+package com.tanach.application.views.rules;
+
+import com.tanach.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Rules")
+@Route(value = "rules", layout = MainLayout.class)
+public class RulesView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public RulesView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
